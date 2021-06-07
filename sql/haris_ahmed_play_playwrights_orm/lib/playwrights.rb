2a0b6df1fc957e0ee019ef59bb8c9df48b6dc2e9
hahmed1523 <hahmed1523@gmail.com>
2021-06-07 16:24:44 -0400

    Completed Playwright class

--- sql/haris_ahmed_play_playwrights_orm/lib/playwrights.rb
+++ sql/haris_ahmed_play_playwrights_orm/lib/playwrights.rb
@@ -3,8 +3,22 @@ require_relative "./plays.rb"
 
 class Playwright
 
+    attr_accessor :name, :birth_year
+
     def self.all
         data = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
+        data.map {|datum| Playwright.new(datum)}
+    end
+
+    def self.find_by_name(name)
+        data = PlayDBConnection.instance.execute(<<-SQL, name)
+            SELECT 
+                *
+            FROM
+                playwrights
+            WHERE
+                name = ?
+            SQL
         data.map {|datum| Playwright.new(datum)}
     end
 
@@ -12,6 +26,42 @@ class Playwright
         @id = options['id']
         @name = options['name']
         @birth_year = options['birth_year']
+    end
+
+    def create
+        raise "#{self} already in database" if @id 
+        PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year)
+            INSERT INTO
+                playwrights (name, birth_year)
+            VALUES
+                (?,?)
+        SQL
+        @id = PlayDBConnection.instance.last_insert_row_id
+    end
+
+    def update
+        raise "#{self} not in database" unless @id 
+        PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year, @id)
+            UPDATE
+                playwrights
+            SET
+                name = ?, birth_year = ?
+            WHERE
+                id = ?
+        SQL
+    end
+
+    def get_plays
+        raise "#{self} not in database" unless @id
+        data = PlayDBConnection.instance.execute(<<-SQL, @name)
+            SELECT 
+                *
+            FROM
+                plays
+            WHERE
+                playwright_id = (SELECT id FROM playwrights WHERE name = ?)
+            SQL
+        data.map {|datum| Play.new(datum)}
     end
 
 end
