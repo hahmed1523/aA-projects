24bf6ecb018b5d3d584b901ae9621c53efeea602
hahmed1523 <hahmed1523@gmail.com>
2021-06-07 14:46:44 -0400

    Tested the connection of Playwright class

--- sql/haris_ahmed_play_playwrights_orm/lib/playwrights.rb
+++ sql/haris_ahmed_play_playwrights_orm/lib/playwrights.rb
@@ -1,3 +1,17 @@
 require 'sqlite3'
-require_relative "plays.rb"
+require_relative "./plays.rb"
 
+class Playwright
+
+    def self.all
+        data = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
+        data.map {|datum| Playwright.new(datum)}
+    end
+
+    def initialize(options)
+        @id = options['id']
+        @name = options['name']
+        @birth_year = options['birth_year']
+    end
+
+end
