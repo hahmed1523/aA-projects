70c638d2cfe41ce451d6ab24cc0271d8a124058f
hahmed1523 <hahmed1523@gmail.com>
2021-06-10 16:08:13 -0400

    Added Question_Follows class

---
+++ sql/haris_ahmed_aa_questions/lib/question_follows.rb
@@ -0,0 +1,53 @@
+require_relative  './connect.rb'
+
+# Interact with Question_follows table with Ruby objects
+
+class Question_Follow
+    attr_accessor :user_id, :question_id
+
+    def self.all
+        data = QuestionsDatabase.instance.execute("SELECT * FROM question_follows")
+        data.map { |datum| Question_Follow.new(datum)}
+    end
+
+    def self.find_by_id (id)
+        data = QuestionsDatabase.instance.execute(<<-SQL, id)
+            SELECT
+                *
+            FROM
+                question_follows
+            WHERE id = ?
+        SQL
+
+        Question_Follow.new(data[0])
+    end 
+
+    def initialize(options)
+        @id = options['id']
+        @user_id = options['user_id']
+        @question_id = options['question_id']
+    end
+
+    def create
+        raise "#{self} already in database" if @id
+        QuestionsDatabase.instance.execute(<<-SQL, @user_id, @question_id)
+            INSERT INTO
+                question_follows (user_id, question_id)
+            VALUES
+                (?, ?)
+        SQL
+        @id = QuestionsDatabase.instance.last_insert_row_id
+    end
+
+    def update
+        raise "#{self} not in database" unless @id
+        QuestionsDatabase.instance.execute(<<-SQL, @user_id, @question_id, @id)
+            UPDATE
+                question_follows
+            SET
+                user_id = ?, question_id = ?
+            WHERE
+                id = ?
+        SQL
+    end
+end
