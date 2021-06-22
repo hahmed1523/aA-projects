7a5a12768329b84a5064a61e2edc41c2d15e4e08
hahmed1523 <hahmed1523@gmail.com>
2021-06-22 09:59:09 -0400

    Added Houses table and House model

---
+++ sql/haris_ahmed_intro_to_rails/project/app/models/house.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: houses
+#
+#  id         :bigint           not null, primary key
+#  address    :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class House < ApplicationRecord
+    #must have address
+    validates :address, presence: true 
+end
--- sql/haris_ahmed_intro_to_rails/project/db/schema.rb
+++ sql/haris_ahmed_intro_to_rails/project/db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_18_145926) do
+ActiveRecord::Schema.define(version: 2021_06_22_134956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "houses", force: :cascade do |t|
+    t.string "address", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "people", force: :cascade do |t|
     t.string "name", null: false
