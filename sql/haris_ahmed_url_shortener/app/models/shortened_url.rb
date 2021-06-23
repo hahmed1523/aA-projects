8aa1f16e869e5deb818603bea436078a755822d8
hahmed1523 <hahmed1523@gmail.com>
2021-06-23 10:43:25 -0400

    Added shortened_urls table and ShortenedUrl model

---
+++ sql/haris_ahmed_url_shortener/app/models/shortened_url.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: shortened_urls
+#
+#  id         :bigint           not null, primary key
+#  long_url   :string           not null
+#  short_url  :string           not null
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class ShortenedUrl < ApplicationRecord
+    validates :long_url, :short_url, :user_id, presence: true
+    validates :long_url, :short_url, uniqueness: true 
+    
+    
+end
