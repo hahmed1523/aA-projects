6679892caf77bd1f622998e1ad801e7e0a6bf140
hahmed1523 <hahmed1523@gmail.com>
2021-07-07 11:38:55 -0400

    Added AnswerChoices table and AnswerChoice model

---
+++ sql/haris_ahmed_polls_app/app/models/answer_choice.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: answer_choices
+#
+#  id          :bigint           not null, primary key
+#  question_id :integer          not null
+#  text        :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+class AnswerChoice < ApplicationRecord
+    validates :question_id, :text, presence: true 
+    
+end
--- sql/haris_ahmed_polls_app/db/schema.rb
+++ sql/haris_ahmed_polls_app/db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_07_151643) do
+ActiveRecord::Schema.define(version: 2021_07_07_153306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answer_choices", force: :cascade do |t|
+    t.integer "question_id", null: false
+    t.string "text", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answer_choices_on_question_id"
+  end
 
   create_table "polls", force: :cascade do |t|
     t.integer "user_id", null: false
