85f8f252db12b11344b49ece91a7b11d3ccef5f4
hahmed1523 <hahmed1523@gmail.com>
2021-07-07 11:20:06 -0400

    Added Questions table and Question model

---
+++ sql/haris_ahmed_polls_app/app/models/question.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: questions
+#
+#  id         :bigint           not null, primary key
+#  poll_id    :integer          not null
+#  text       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Question < ApplicationRecord
+    validates :poll_id, :text, presence: true 
+end
