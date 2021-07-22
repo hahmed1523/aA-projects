780c13f589a81a7ee4a86d3ec183013fa5698891
hahmed1523 <hahmed1523@gmail.com>
2021-07-22 11:33:54 -0400

    Added ArtworkShare table and model

---
+++ rails/haris_ahmed_art_share/db/migrate/20210722152439_add_index_to_artwork_shares.rb
@@ -0,0 +1,7 @@
+class AddIndexToArtworkShares < ActiveRecord::Migration[5.2]
+  def change
+    add_index :artwork_shares, :artwork_id
+    add_index :artwork_shares, :viewer_id
+    add_index :artwork_shares, [:viewer_id, :artwork_id], unique: true 
+  end
+end
