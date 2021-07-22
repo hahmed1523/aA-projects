9107c3043a9c8af1143a8984c537b66274450bc2
hahmed1523 <hahmed1523@gmail.com>
2021-07-22 15:55:33 -0400

    Completed Artworks Controller

--- rails/haris_ahmed_art_share/app/controllers/artworks_controller.rb
+++ rails/haris_ahmed_art_share/app/controllers/artworks_controller.rb
@@ -3,6 +3,42 @@ class ArtworksController < ApplicationController
         render json: Artwork.all 
     end
 
+    def create
+        artwork = Artwork.new(artwork_params)
+
+        if artwork.save
+            render json: artwork
+        else
+            render json: artwork.errors.full_messages, status: :unprocessable_entity
+        end
+
+    end
+
+    def show
+        artwork = Artwork.find(params[:id])
+        
+        render json: artwork
+    end
+
+    def update
+        artwork = Artwork.find(params[:id])
+
+        if artwork.update(artwork_params)
+            render json: artwork
+        else
+            render json: artwork.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        artwork = Artwork.find(params[:id])
+
+        if artwork.destroy 
+            render json: artwork
+        else
+            render json  artwork.errors.full_messages, status: :unprocessable_entity
+        end
+    end
 
 
 
