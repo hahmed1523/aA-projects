897cba0fc436fce014f712c992e067cf3bc259dc
hahmed1523 <hahmed1523@gmail.com>
2021-07-22 11:16:27 -0400

    Added Associations to User and Artwork

--- rails/haris_ahmed_art_share/app/models/artwork.rb
+++ rails/haris_ahmed_art_share/app/models/artwork.rb
@@ -12,5 +12,10 @@
 class Artwork < ApplicationRecord
     validates :title, :image_url, :artist_id, presence: true 
     validates :artist_id, uniqueness: { scope: :title }
+
+    belongs_to :artist,
+        primary_key: :id, #user's id
+        foreign_key: :artist_id,
+        class_name: :User 
     
 end
--- rails/haris_ahmed_art_share/app/models/user.rb
+++ rails/haris_ahmed_art_share/app/models/user.rb
@@ -9,4 +9,11 @@
 #
 class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true 
+
+    has_many :artworks,
+        dependent: :destroy,
+        primary_key: :id, #user's id
+        foreign_key: :artist_id,
+        class_name: :Artwork 
+        
 end
