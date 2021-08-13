0cb945b8a4bd255b3923fd19e68313adcd4619b7
hahmed1523 <hahmed1523@gmail.com>
2021-08-13 12:25:01 -0400

    Added Session new view

---
+++ rails/haris_ahmed_99_cats_II/app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+    def create 
+        user = User.find_by_credentials(
+            params[:user][:username],
+            params[:user][:password]
+        )
+
+        if user.nil? 
+            render json: 'Credentials were wrong'
+        else
+            render json: "Welcome back #{user.username}!"
+        end
+
+    end
+
+    def new
+        render :new 
+    end
+
+end
