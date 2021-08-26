c52ebfea0f333840c2dd11752088d1f1f02f1c28
hahmed1523 <hahmed1523@gmail.com>
2021-08-26 10:29:55 -0400

    Added new view for band

--- rails/haris_ahmed_music_app/app/controllers/bands_controller.rb
+++ rails/haris_ahmed_music_app/app/controllers/bands_controller.rb
@@ -8,7 +8,32 @@ class BandsController < ApplicationController
     def show
         @band = Band.find_by(id: params[:id])
 
-        render :show
+        if @band 
+            render :show
+        else 
+            redirect_to bands_url
+        end
+    end
+
+    def new
+        @band = Band.new
+        render :new 
+    end
+
+    def create
+        @band = Band.new(band_params)
+
+        if @band.save
+            redirect_to band_url(@band)
+        else
+            flash.now[:errors] = @band.errors.full_messages
+            render :new 
+        end
+    end
+
+    private
+    def band_params
+        params.require(:band).permit(:name)
     end
 
 end
