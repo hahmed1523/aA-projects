2fb66e069f4c03b160298bc580603d919e705073
hahmed1523 <hahmed1523@gmail.com>
2021-08-26 16:24:49 -0400

    Completed the base for bands

--- rails/haris_ahmed_music_app/app/controllers/sessions_controller.rb
+++ rails/haris_ahmed_music_app/app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
             render :new 
         else
             login!(user)
-            redirect_to user_url(user)
+            redirect_to bands_url
         end
     end
 
