c11c93f0b81b3bb4ac13b88485338524d952c3d7
hahmed1523 <hahmed1523@gmail.com>
2021-09-27 15:06:07 -0400

    Added PENDING it statements to User test file

--- rails/haris_ahmed_goal_tracker/spec/models/user_spec.rb
+++ rails/haris_ahmed_goal_tracker/spec/models/user_spec.rb
@@ -1,5 +1,38 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id               :bigint           not null, primary key
+#  email            :string           not null
+#  password_digest  :string           not null
+#  session_token    :string           not null
+#  admin            :boolean          default(FALSE), not null
+#  activated        :boolean          default(FALSE), not null
+#  activation_token :string           not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validations' do
+    it "should validate presence of email"
+    it "should validate presence of password_digest"
+    it "should validate presence of session_token"
+    it "should validate presence of admin"
+    it "should validate presence of activated"
+    it "should validate presence of activation_token"
+  
+  end
+
+  describe 'associations' do
+  
+  end
+
+  describe 'class methods' do 
+  
+  end
+
+
 end
