cc0715804d6a8eb86034f9eaf92b3f605560b401
hahmed1523 <hahmed1523@gmail.com>
2021-09-30 10:06:33 -0400

    tested Users#index in controller

--- rails/haris_ahmed_goal_tracker/app/controllers/users_controller.rb
+++ rails/haris_ahmed_goal_tracker/app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+
+    def index
+        render :index 
+    end
 end
--- rails/haris_ahmed_goal_tracker/spec/controllers/users_controller_spec.rb
+++ rails/haris_ahmed_goal_tracker/spec/controllers/users_controller_spec.rb
@@ -2,4 +2,28 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
+    describe 'GET #index' do 
+        it 'renders the user\'s index' do 
+            get :index 
+            expect(response).to be_success
+            expect(response).to render_template(:index)
+        end
+    end
+
+
+    describe 'GET #show' do 
+        it 'renders the show template'
+    end
+
+
+    describe 'POST #create' do 
+        context 'with invalid params' do 
+            it 'renders the new template'
+        end
+
+        context 'with valid params' do 
+            it 'redirects to user page'
+        end
+    end
+
 end
