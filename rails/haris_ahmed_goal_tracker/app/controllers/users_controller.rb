42422665143fc94ef26d43cf8395b9be7a42c819
hahmed1523 <hahmed1523@gmail.com>
2021-09-30 13:50:14 -0400

    updated user controller tests

--- rails/haris_ahmed_goal_tracker/app/controllers/users_controller.rb
+++ rails/haris_ahmed_goal_tracker/app/controllers/users_controller.rb
@@ -6,7 +6,29 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.find_by(id: params[:id])
+        @user = User.find(params[:id])
         render :show
+    end
+
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            redirect_to user_url(@user)
+        else
+            flash.now[:errors] = @user.errors.full_messages
+            render :new
+        end
+    end
+
+    def new
+        @user = User.new 
+        render :new  
+    end
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:email, :password)
     end
 end
--- rails/haris_ahmed_goal_tracker/spec/controllers/users_controller_spec.rb
+++ rails/haris_ahmed_goal_tracker/spec/controllers/users_controller_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
+    let(:valid_params) do 
+        {
+            # id: '1',
+            email: 'create_test',
+            password: 'password'
+        }
+    end
+
     describe 'GET #index' do 
         it 'renders the user\'s index' do 
             get :index 
@@ -20,14 +28,26 @@ RSpec.describe UsersController, type: :controller do
     end
 
 
-    describe 'POST #create' do 
-        context 'with invalid params' do 
-            it 'renders the new template'
-        end
+    # describe 'POST #create' do 
+    #     context 'with invalid params' do 
+    #         it 'renders the new template' do 
+    #             post :create, params: { user: { email: '' } }
+    #             expect(response).to render_template(:new)
+    #         end
+    #     end
 
-        context 'with valid params' do 
-            it 'redirects to user page'
-        end
-    end
+    #     context 'with valid params' do
+
+            
+            
+    #         it 'creates a new user' do 
+    #             # post :create, params: { user: valid_params }
+    #             # expect(response).to redirect_to(user_url(User.find_by(id: valid_params.id)))
+    #             expect{
+    #                 post :create, params: {  user: valid_params}
+    #             }.to change(User, :count).by(1)
+    #         end
+    #     end
+    # end
 
 end
