532f6d93ad785cb0f25aa0c5cb77507457b460e4
hahmed1523 <hahmed1523@gmail.com>
2021-10-01 13:48:42 -0400

    Added user request specs for show, new

--- rails/haris_ahmed_goal_tracker/app/controllers/users_controller.rb
+++ rails/haris_ahmed_goal_tracker/app/controllers/users_controller.rb
@@ -6,13 +6,32 @@ class UsersController < ApplicationController
         render :index  
     end
 
+    def show
+        @user = User.find_by(id: params[:id])
+
+        if @user 
+            render :show
+        else
+            flash.now[:errors] = "User is not found"
+            redirect_to users_path 
+        end
+    end
+
+    def new 
+        @user = User.new 
+        render :new 
+    end
+
     def create
         @user = User.new(user_params)
 
         if @user.save
-            render json: @user, status: :created, location: @user 
+            #render json: @user, status: :created, location: @user 
+            redirect_to user_url(@user)
         else
-            render json: @user.errors.full_messages, status: :unprocessable_entity
+            #render json: @user.errors.full_messages, status: :unprocessable_entity
+            flash.now[:errors] = @user.errors.full_messages
+            render :new
         end
     end
 
--- rails/haris_ahmed_goal_tracker/spec/requests/users_request_spec.rb
+++ rails/haris_ahmed_goal_tracker/spec/requests/users_request_spec.rb
@@ -10,6 +10,29 @@ RSpec.describe "Users", type: :request do
         end
     end
 
+    describe "GET #show" do 
+        it 'renders the show template' do 
+            user = FactoryBot.create(:user)
+            get user_path(user)
+            expect(response).to render_template(:show)
+        end
+
+        context 'if the user does not exist' do 
+            it 'is not a success' do 
+                get user_path(-1)
+                expect(flash[:errors]).to include("User is not found")
+                expect(response).to redirect_to(users_url)
+            end 
+        end
+    end
+
+    describe "GET #new" do 
+        it 'render new template' do 
+            get new_user_path
+            expect(response).to render_template(:new)
+        end
+    end
+
     describe "POST #create" do 
         let(:valid_params) do 
             {
@@ -20,26 +43,57 @@ RSpec.describe "Users", type: :request do
             }
         end
 
-        it 'add has HTTP status 201 of created' do 
-            
-            #post users_path, params: {user: { email: 'post_test', password: 'password' }}
-            post users_path, params: valid_params
-            expect(response.status).to eq(201)           
+        let(:invalid_params) do 
+            {
+                user: {
+                    password: 'password'
+                }
+            }
         end
 
-        it 'add new record to database' do 
-            expect{ post users_path, params: valid_params }.to change(User, :count).by(+1)
+        context 'valid params' do 
+
+            it 'add new record to database' do 
+                expect{ post users_path, params: valid_params }.to change(User, :count).by(+1)
+            end
+
+            it 'has HTTP status 302 of redirect' do 
+                
+                post users_path, params: valid_params
+                expect(response.status).to eq(302)           
+            end
+
+            it 'redirects to user page' do 
+                post users_path, params: valid_params
+                expect(response).to redirect_to(user_url(User.last))
+            end
+
+        end
+
+        context 'invalid params' do 
+            # it 'has HTTP status 422 of unprocessable entity' do 
+            #     post users_path, params: invalid_params
+            #     expect(response.status).to eq(422) 
+            # end
+
+            it 'does not add new record to database' do 
+                expect{ post users_path, params: invalid_params }.to change(User, :count).by(0)
+            end
+
+            
+            it 'redirects to new view' do
+                post users_path, params: invalid_params
+                expect(response).to render_template(:new)
+            end
+
+            it 'has error in flash' do 
+                post users_path, params: invalid_params
+                expect(flash[:errors]).to eq(['Email can\'t be blank'])
+            end
+        
         end
 
         
     end
 
-    # # make a get request
-    # get users_path
-
-    # # make a post request
-    # post users_path, user: {email: 'post_test', password: 'password'}
-
-    # # check the response body and/or status
-    # expect(response).to have_http_status(200)
 end
