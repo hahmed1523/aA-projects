0ce1121ab87843681616b7056a3c33cf9ae0e4a2
hahmed1523 <hahmed1523@gmail.com>
2021-10-04 14:15:38 -0400

    started the features/view tests

---
+++ rails/haris_ahmed_goal_tracker/spec/features/auth_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+require 'rails_helper'
+
+feature "user features", type: :feature do 
+
+    feature 'the signup process' do 
+
+        scenario 'has a new user page' do 
+            visit new_user_url 
+            expect(page).to have_content 'New User'
+        end
+
+        feature 'signing up a user' do 
+
+            before(:each) do 
+                visit new_user_url 
+                fill_in 'email', with: 'new_email_test'
+                fill_in 'password', with: 'password'
+                click_on 'Create User'
+            end
+
+            scenario 'shows username on the homepage after signup' do
+                expect(page).to have_content 'new_email_test'
+            end
+
+        end
+
+    end
+
+end
