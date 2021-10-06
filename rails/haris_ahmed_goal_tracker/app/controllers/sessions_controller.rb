ef53adeff78b12f692518e5ac57f22978e1fa25f
hahmed1523 <hahmed1523@gmail.com>
2021-10-06 12:30:58 -0400

    Completed Session Controller with tests

--- rails/haris_ahmed_goal_tracker/app/controllers/sessions_controller.rb
+++ rails/haris_ahmed_goal_tracker/app/controllers/sessions_controller.rb
@@ -1,5 +1,32 @@
 class SessionsController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
     def new 
         render :new 
     end
+
+    def create 
+        @user = User.find_by_credentials(
+            params[:user][:email],
+            params[:user][:password]
+        )
+        if @user.nil? 
+            #render json: "Invalid Credentials"
+            flash.now[:errors] = ["Invalid credentials."]
+            render :new 
+        else
+            #render json: @user
+            login_user!(@user)
+            redirect_to root_url 
+        end
+        #render json: params[:user][:email] 
+    end
+
+    def destroy 
+        current_user.reset_session_token!
+        session[:session_token] = nil 
+
+        redirect_to new_session_url
+    end
+
 end
--- rails/haris_ahmed_goal_tracker/spec/requests/session_request_spec.rb
+++ rails/haris_ahmed_goal_tracker/spec/requests/session_request_spec.rb
@@ -6,6 +6,59 @@ RSpec.describe "Session", type: :request do
             get new_session_path
             expect(response).to render_template(:new)
         end
+    end
 
+    describe "POST #create" do 
+
+        user = FactoryBot.create(:user)
+
+        context 'valid params' do 
+           
+            it 'logs in user' do 
+                post session_path, params: {user: {email: user.email, password: user.password}}
+                updated_user = User.find(user.id)
+                token = updated_user.session_token
+                expect(session[:session_token]).to eq(token)
+            end
+
+            it 'redirects to root' do 
+                post session_path, params: {user: {email: user.email, password: user.password}}
+                expect(response.status).to eq(302)
+                expect(response).to redirect_to(root_url)
+            end
+        end
+
+        context 'invalid params' do 
+
+           
+            it 'adds error to flash' do 
+                post session_path, params: {user: {email: "blah", password: user.password}}
+                expect(flash[:errors]).to include("Invalid credentials.")
+            end
+
+            it 'renders new template' do 
+                post session_path, params: {user: {email: "blah", password: user.password}}
+                expect(response).to render_template(:new)
+            end
+        end
+    end
+
+    describe "DELETE #destroy" do 
+        user = FactoryBot.create(:user)
+
+        it 'make session[:session_token] nil' do
+            #expect(session[:session_token].nil?).to eq(true)
+            post session_path, params: {user: {email: user.email, password: user.password}}
+            expect(session[:session_token].nil?).to eq(false)
+            delete session_url 
+            expect(session[:session_token].nil?).to eq(true)
+        end
+
+        it 'redirect_to new_session_url' do 
+            post session_path, params: {user: {email: user.email, password: user.password}}
+            delete session_url
+            expect(response).to redirect_to(new_session_url)
+
+        end
     end
 end
