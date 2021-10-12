fb6df637ff3bfe111c6e810a4d91f34d690025e5
hahmed1523 <hahmed1523@gmail.com>
2021-10-12 16:58:35 -0400

    Passed feature tests for signing up and logging in

--- rails/haris_ahmed_goal_tracker/spec/features/auth_spec.rb
+++ rails/haris_ahmed_goal_tracker/spec/features/auth_spec.rb
@@ -28,6 +28,38 @@ feature "user features", type: :feature do
             end
 
         end
+        let(:user) { FactoryBot.create(:user) }
+        let(:log_on){
+            
+            visit new_session_url
+            fill_in 'Email', with: user.email 
+            fill_in 'Password', with: user.password 
+            click_on 'Sign In'
+        }
+        feature 'logging in' do 
+            scenario 'shows username on the homepage after login' do 
+                log_on 
+                expect(page).to have_content user.email 
+
+            end
+        end
+
+        feature 'logging out' do 
+            scenario 'begins with a logged out state' do 
+                visit root_url 
+                expect(page).not_to have_content user.email 
+                expect(page).not_to have_selector(:link_or_button, 'Sign Out')
+            end
+
+            scenario 'doesn\'nt show username on the homepage after logout' do 
+                log_on 
+                expect(page).to have_content user.email 
+                click_on 'Sign Out'
+                expect(page).not_to have_content user.email 
+            end
+
+            
+        end
 
     end
 
