f0ccbc96375d5a988eb8dbed54a277a3dd25ea77
hahmed1523 <hahmed1523@gmail.com>
2021-10-13 15:51:24 -0400

    Tested create request for Goals

--- rails/haris_ahmed_goal_tracker/app/controllers/goals_controller.rb
+++ rails/haris_ahmed_goal_tracker/app/controllers/goals_controller.rb
@@ -1,5 +1,6 @@
 class GoalsController < ApplicationController
     skip_before_action :verify_authenticity_token
+    before_action :require_user!
 
     def show
         @goal = Goal.find_by(id: params[:id])
@@ -15,6 +16,18 @@ class GoalsController < ApplicationController
     def new 
         @goal = Goal.new 
         render :new 
+    end
+
+    def create
+        @goal = Goal.new(goal_params)
+        @goal.user_id = current_user.id 
+
+        if @goal.save
+            redirect_to goal_url(@goal)
+        else
+            flash.now[:errors] = @goal.errors.full_messages
+            render :new
+        end
     end
 
 
--- rails/haris_ahmed_goal_tracker/spec/requests/goals_request_spec.rb
+++ rails/haris_ahmed_goal_tracker/spec/requests/goals_request_spec.rb
@@ -4,6 +4,10 @@ RSpec.describe "Goals", type: :request do
 
     let(:user) { FactoryBot.create(:user) }
 
+    before do
+        allow_any_instance_of(GoalsController).to receive(:current_user).and_return(user) 
+    end
+
     describe "GET #show" do 
         let(:goal) { FactoryBot.create(:goal, user_id: user.id) }
         it 'renders the show template' do 
@@ -32,6 +36,53 @@ RSpec.describe "Goals", type: :request do
             get new_goal_path 
             expect(response).to render_template(:new)
         end
+    end
+
+    describe "POST #create" do 
+        let(:valid_params) do 
+            {
+                goal: {
+                    title: "Basketball",
+                    details: "Play Bball"
+                }
+            }
+        end
+
+        let(:invalid_params) do 
+            {
+                goal: {
+                    title: "Basketball"
+                }
+            }
+        end
+
+        context 'valid params' do 
+
+            it 'add new record to database' do 
+                expect{ post goals_url, params: valid_params }.to change(Goal, :count).by(+1)
+            end
+
+            it 'redirects to goal page' do 
+                post goals_url, params: valid_params
+                expect(response).to redirect_to(goal_url(Goal.last))
+            end
+        end
+
+        context 'invalid params' do 
+            it 'does not add new record to database' do 
+                expect{ post goals_url, params: invalid_params }.to change(Goal, :count).by(0)
+            end
+            
+            before(:each) { post goals_url, params: invalid_params }
+            
+            it 'redirects to new view' do 
+                expect(response).to render_template(:new)
+            end
+            it 'has error in flash' do 
+                expect(flash[:errors]).to include("Details can't be blank") 
+            end
+        end
+
     end
 
 end
