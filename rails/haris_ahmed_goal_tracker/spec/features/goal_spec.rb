918c11f7682b8de5e9f87a00cabc0e965d84b044
hahmed1523 <hahmed1523@gmail.com>
2021-10-18 15:21:03 -0400

    Added test to add new goal

--- rails/haris_ahmed_goal_tracker/spec/features/goal_spec.rb
+++ rails/haris_ahmed_goal_tracker/spec/features/goal_spec.rb
@@ -6,22 +6,21 @@ feature "goal features", type: :feature do
     let(:user1) { FactoryBot.create(:user) }
     let(:user2) { FactoryBot.create(:user) }
 
-    
+    let(:sign_on_u1) {
+        visit new_session_url 
+        fill_in 'Email', with: user1.email
+        fill_in 'Password', with: user1.password 
+        click_on 'Sign In' 
+    }
+
+    let(:sign_on_u2) {
+        visit new_session_url 
+        fill_in 'Email', with: user2.email
+        fill_in 'Password', with: user2.password 
+        click_on 'Sign In' 
+    }   
 
     feature 'show message that user has no goals or list their goals if they have some' do 
-        let(:sign_on_u1) {
-            visit new_session_url 
-            fill_in 'Email', with: user1.email
-            fill_in 'Password', with: user1.password 
-            click_on 'Sign In' 
-        }
-
-        let(:sign_on_u2) {
-            visit new_session_url 
-            fill_in 'Email', with: user2.email
-            fill_in 'Password', with: user2.password 
-            click_on 'Sign In' 
-        }
 
         scenario 'be on user\'s page' do 
             sign_on_u1
@@ -38,6 +37,17 @@ feature "goal features", type: :feature do
             sign_on_u2
             expect(page).not_to have_content("You have no goals")
             expect(page).to have_content(goal.title)
+        end
+    end
+
+    feature 'add a new goal' do 
+        scenario 'adds new goal and goes to the goal page' do 
+            sign_on_u2
+            click_on 'Add Goal'
+            fill_in 'Title', with: 'New Goal Test'
+            fill_in 'Details', with: 'This is the detail'
+            click_on 'Create Goal'
+            expect(page).to have_content('New Goal Test')
         end
     end
     
