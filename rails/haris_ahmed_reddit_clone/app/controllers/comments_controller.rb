686c24af9d353da73e3b1c7974344b2b512210ec
hahmed1523 <hahmed1523@gmail.com>
2021-11-09 12:23:53 -0500

    Added require user before action to comments controlelr

--- rails/haris_ahmed_reddit_clone/app/controllers/comments_controller.rb
+++ rails/haris_ahmed_reddit_clone/app/controllers/comments_controller.rb
@@ -1,4 +1,9 @@
 class CommentsController < ApplicationController
+    before_action :require_user!, only: [:create, :new]
+
+    def new
+        @comment = Comment.new(post_id: params[:post_id])
+    end
 
     def create 
         @comment = Comment.new(comment_params)
@@ -14,6 +19,6 @@ class CommentsController < ApplicationController
 
     private
     def comment_params
-        params.require(:comment).permit(:user_id, :post_id, :body)
+        params.require(:comment).permit(:post_id, :body)
     end
 end
