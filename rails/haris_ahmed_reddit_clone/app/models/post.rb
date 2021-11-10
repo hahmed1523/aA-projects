f0e9a404d3d957e3deeddfcca1df7038955b551c
hahmed1523 <hahmed1523@gmail.com>
2021-11-10 11:31:55 -0500

    Tested comments_by_parent method for post model

--- rails/haris_ahmed_reddit_clone/app/models/post.rb
+++ rails/haris_ahmed_reddit_clone/app/models/post.rb
@@ -34,4 +34,14 @@ class Post < ApplicationRecord
         
         has_many :comments, inverse_of: :post 
 
+        def comments_by_parent 
+            comments_by_parent = Hash.new { |hash, key| hash[key] = [] }
+
+            self.comments.includes(:author).each do |comment|
+                comments_by_parent[comment.parent_comment_id] << comment 
+            end
+
+            comments_by_parent 
+        end
+
 end
--- rails/haris_ahmed_reddit_clone/spec/models/post_spec.rb
+++ rails/haris_ahmed_reddit_clone/spec/models/post_spec.rb
@@ -17,6 +17,8 @@ RSpec.describe Post, type: :model do
 
   let(:user) { FactoryBot.create(:user) }
   let(:sub) { FactoryBot.create(:sub, moderator_id: user.id) }
+
+
   describe 'validations' do 
 
     #Validating presence
@@ -30,6 +32,31 @@ RSpec.describe Post, type: :model do
     it { should have_many(:post_subs)}
     it { should have_many(:subs) }
     it { should have_many(:comments) }
+  end
+
+
+  describe 'class methods' do 
+    let(:post1) { Post.create(title: "Post1", content: "this is content", author_id: user.id) }
+
+    describe 'comments_by_parent' do 
+
+        let!(:parent_comment1) { Comment.create(user_id: user.id, post_id: post1.id, 
+          body: "this is parent1 comment", parent_comment_id: nil) }
+        
+        let!(:parent_comment2) { Comment.create(user_id: user.id, post_id: post1.id, 
+          body: "this is parent2 comment", parent_comment_id: nil)}
+
+        let!(:cc_p1) { Comment.create(user_id: user.id, post_id: post1.id, 
+          body: "this is child1 for parent1 comment", parent_comment_id: parent_comment1.id)}
+        
+
+      it 'should return a hash of all the posts comments with the key being the parent_comment _id' do    
+
+        expect(post1.comments_by_parent[nil].length).to eq(2)
+        expect(post1.comments_by_parent[parent_comment1.id].length).to eq(1)
+
+      end
+    end
   end
 
 
